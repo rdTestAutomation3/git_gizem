0ec7a3c2ba35a37612a8e0df668e53a48663874f
Gizem <gizsalihoglu@gmail.com>
2024-10-13 21:45:10 +0300

    modelleme

---
+++ personelModelleme.java
@@ -0,0 +1,8 @@
+public class personelModelleme {
+    private String name;
+    private String surname;
+    private String company;
+    private String title;
+    private int age;
+    private int CompanyID
+}
